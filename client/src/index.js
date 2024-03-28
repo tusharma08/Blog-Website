80a294ce7f8f25be595beb305fea8c092d86cb25
Tushar Sharma <tushar.sh08@gmail.com>
2024-03-28 14:13:44 +0530

    modified:   client/package-lock.json 	modified:   client/package.json 	new file:   client/src/components/Footer.jsx 	new file:   client/src/components/Header.jsx 	new file:   client/src/components/Layout.jsx 	modified:   client/src/index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import './index.css';
+import Layout from './components/Layout';
+import ErrorPage from './pages/ErrorPage'
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout/>,
+    errorElement: <ErrorPage/>,
+    children: [
+      {}
+    ]
+  }
+])
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <h1>Hello </h1>
+    <RouterProvider router={router}/>
   </React.StrictMode>
 );
