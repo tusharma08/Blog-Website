90ef1162bfcef67281ce298a5fe396b91b27e953
Tushar Sharma <tushar.sh08@gmail.com>
2024-03-29 15:31:41 +0530

    modified:   client/src/components/Footer.jsx 	modified:   client/src/index.css 	modified:   client/src/pages/ErrorPage.jsx

--- client/src/pages/ErrorPage.jsx
+++ client/src/pages/ErrorPage.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
 const ErrorPage = () => {
   return (
-    <div>ErrorPage</div>
+    <section className='error_page'>
+      <div className="center">
+        <Link to= "/" className='btn primary'>Go Back!</Link>
+        <h1>Page Not Found!!</h1>
+      </div>
+    </section>
   )
 }
 
