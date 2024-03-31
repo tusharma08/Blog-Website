8d8b80ad7f30267d0f790dd315b51ba92bdbc841
Tushar Sharma <tushar.sh08@gmail.com>
2024-03-31 12:41:50 +0530

    modified:   client/package-lock.json 	modified:   client/package.json 	modified:   client/src/index.css 	modified:   client/src/pages/CreatePost.jsx 	modified:   client/src/pages/EditPost.jsx

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -1,8 +1,51 @@
-import React from 'react'
+import React, { useState } from 'react'
+import ReactQuill from 'react-quill'
+import 'react-quill/dist/quill.snow.css'
 
 const CreatePost = () => {
+  const [title, setTitle] = useState('')
+  const [category, setCategory] = useState('UnCategorized')
+  const [desc, setDesc] = useState('')
+  const [thumbnail, setThumbnail] = useState('')
+
+  const modules = {
+    toolbar: [
+      [{ 'header' : [1,2,3,4,5,6,false]}],
+      ['bold', 'italic', 'underline', 'strike', 'blockqoute'],
+      [{'list' : 'ordered'}, {'list':'bullet'}, {'indent':
+    -1}, {'indent' : '+1'}],
+    ['link','image'],
+    ['clean']
+    ]
+  }
+
+  const formats = [
+    'header',
+    'bold', 'italic', 'underline', 'strike', 'blockqoute',
+    'list', 'bullet', 'indent',
+    'link', 'image'
+  ]
+
+  const POST_CATEGRIES = ["Select", "Argiculture", "Business", "Education", "Entertainment", "Art", "Investment", "Weather", "Uncategorized"]
+
   return (
-    <div>CreatePost</div>
+    <section className="create_post">
+      <div className="container">
+        <h2>Create Post</h2>
+        <p className="form_error_message">This is Error Message!</p>
+        <form action="" className='form create_post_form'>
+          <input type="text" name="" placeholder='Title' value={title} onChange={e => setTitle(e.target.value)} autoFocus/>
+          <select name="category" value={category} onChange={e => setCategory(e.target.value)}>
+            {
+              POST_CATEGRIES.map(cat => <option key={cat}>{cat}</option>)
+            }
+            </select>
+            <ReactQuill className='editor' modules={modules} formats={formats} value={desc} onChange={setDesc}/>
+            <input type="file" onChange={e =>setThumbnail(e.target.files[0])} accept='png, jpg, jpeg' />
+            <button type='submit' className='btn primary'>Create</button>
+        </form>
+      </div>
+    </section>
   )
 }
 
