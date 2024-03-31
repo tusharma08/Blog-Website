c2460d2dddb920de4be37179189fdd7ec7035809
Tushar Sharma <tushar.sh08@gmail.com>
2024-03-31 15:42:27 +0530

    modified:   client/src/components/Header.jsx 	modified:   client/src/index.css 	modified:   client/src/pages/Dashboard.jsx

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,32 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import Logo from "../images/logo.png";
 import { FaBars } from "react-icons/fa";
 import { AiOutlineClose } from "react-icons/ai";
 
 const Header = () => {
+  const [isNavShowing, setIsNavShowing] = useState(window.innerWidth > 800 ? true : false)
+
+  const closeNavHandler = () => {
+    if(window.innerWidth < 800) {
+      setIsNavShowing(false);
+    } else {
+      setIsNavShowing(true)
+    }
+  }
+
   return (
     <nav>
       <div className="container nav_container">
-        <Link to="/" className="nav_logo">
+        <Link to="/" className="nav_logo" onClick={closeNavHandler}>
           <img src={Logo} alt="Navbar Logo" />
         </Link>
-        <ul className="nav_menu">
+        {isNavShowing && <ul className="nav_menu">
           <li>
-            <Link to="/profile/sdfsdf">Tushar_Sharma</Link>
+            <Link to="/profile/sdfsdf" onClick={closeNavHandler}>Tushar_Sharma</Link>
           </li>
           <li>
-            <Link to="/create">Create_Post</Link>
+            <Link to="/create" onClick={closeNavHandler}>Create_Post</Link>
           </li>
           <li>
-            <Link to="/authors">Authors</Link>
+            <Link to="/authors" onClick={closeNavHandler}>Authors</Link>
           </li>
           <li>
-            <Link to="/logout">Logout</Link>
+            <Link to="/logout" onClick={closeNavHandler}>Logout</Link>
           </li>
-        </ul>
-        <button className="nav_toggle-btn">
-          <AiOutlineClose />
+        </ul>}
+        <button className="nav_toggle-btn" onClick={() => setIsNavShowing(!isNavShowing)}>
+          {isNavShowing ? <AiOutlineClose /> : <FaBars />}
         </button>
       </div>
     </nav>
--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,35 +1,61 @@
-import React, { useState } from 'react'
-import {DUMMY_POSTS} from '../data'
-import {Link} from 'react-router-dom'
+import React, { useState, useEffect } from 'react';
+import { DUMMY_POSTS } from '../data';
+import { Link } from 'react-router-dom';
 
 const Dashboard = () => {
-  const [posts, setPosts] = useState(DUMMY_POSTS)
+ const [posts, setPosts] = useState(DUMMY_POSTS);
+ const [isMobile, setIsMobile] = useState(window.innerWidth < 800);
 
-  return (
+ // Function to truncate the title if it's longer than 20 characters
+ const truncateTitle = (title) => {
+    return isMobile && title.length > 20 ? title.substring(0, 20) + '...' : title;
+ };
+
+ // Effect to update isMobile state when window is resized
+ useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth < 800);
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    // Cleanup function to remove the event listener
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+ }, []);
+
+ return (
     <section className='dashboard'>
       {
-        posts.length ? <div className="container dashboard_container">
-          {
-            posts.map(post => {
-              return <article key={post.id} className='dashboard_post'>
-                <div className="dashboard_post_info">
-                  <div className="dashboard_post_thumbnail">
-                    <img src={post.thumbnail} alt="" />
-                  </div>
-                  <h4>{post.title}</h4>
-                </div>
-                <div className="dashboard_post_actions">
-                  <Link to={`/post/${post.id}`} className='btn sm'>View</Link>
-                  <Link to={`/post/${post.id}/edit`} className='btn sm primary'>Edit</Link>
-                  <Link to={`/post/${post.id}/delete`} className='btn sm danger'>Delete</Link>
-                </div>
-              </article>
-            })
-          }
-        </div> : <h2 className='center'>You have No Post Yet!</h2>
+        posts.length ? (
+          <div className="container dashboard_container">
+            {
+              posts.map(post => {
+                return (
+                 <article key={post.id} className='dashboard_post'>
+                    <div className="dashboard_post_info">
+                      <div className="dashboard_post_thumbnail">
+                        <img src={post.thumbnail} alt="" />
+                      </div>
+                      <h4>{truncateTitle(post.title)}</h4>
+                    </div>
+                    <div className="dashboard_post_actions">
+                      <Link to={`/post/${post.id}`} className='btn sm'>View</Link>
+                      <Link to={`/post/${post.id}/edit`} className='btn sm primary'>Edit</Link>
+                      <Link to={`/post/${post.id}/delete`} className='btn sm danger'>Delete</Link>
+                    </div>
+                 </article>
+                );
+              })
+            }
+          </div>
+        ) : (
+          <h2 className='center'>You have No Post Yet!</h2>
+        )
       }
     </section>
-  )
-}
+ );
+};
 
-export default Dashboard
+export default Dashboard;
